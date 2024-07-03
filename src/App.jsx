effb07a9a1ae26e9d997c4f3ca356db796b62cb7
ferat.goksu <ferat.goksu@ibb.gov.tr>
2024-07-03 16:14:04 +0300

    carousel

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,36 @@
-import React from "react";
-import {createRoot} from "react-dom";
 
+import {createRoot} from "react-dom/client";
+import SearchParams from "./SearchParams";
+import {Link, BrowserRouter,Routes,Route } from "react-router-dom";
+import { QueryClient,QueryClientProvider } from "@tanstack/react-query";
+import Details from "./Details";
 
-
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed),
-  ]);
-};
+const queryClient  = new QueryClient ({
+  defaultOptions : {
+    queries:{
+      staleTime: Infinity,
+      cacheTime: Infinity
+    }
+  }
+  
+})
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockatiel",
-    }),
-    React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mixed" }),
-  ]);
+  return (
+    <BrowserRouter>
+     <QueryClientProvider client={queryClient}>
+    <header>
+      <Link to="/">Adopt ME!</Link>
+    </header>
+    <Routes>
+      <Route path="/details/:id" element={<Details />} />
+      <Route path="/" element={<SearchParams />} />
+    </Routes>
+    </QueryClientProvider>
+  </BrowserRouter>
+  )
 };
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(React.createElement(App));
+root.render(<App />);
